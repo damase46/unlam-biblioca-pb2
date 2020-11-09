7dee4769dbca5b1b52c125547a77fa058eb32ede
marcelo1995tate <mzelaya@alumno.unlam.edu.ar>
2020-11-09 18:49:24 -0300

    addStockByBookId-OK

--- src/test/java/com/unlam/library/services/LibraryServiceTest.java
+++ src/test/java/com/unlam/library/services/LibraryServiceTest.java
@@ -10,5 +10,36 @@ import com.unlam.library.services.LibraryService;
 
 
 public class LibraryServiceTest {
+
+	@Before
+	public void setup() {
+		LibraryService libraryService= LibraryService.getInstance();
+		libraryService.cleanStockBooks();
+	}
+
+	@Test
+	public void addStockByBookId() {
+		LibraryService libraryService= LibraryService.getInstance();
+		libraryService.addStockByBookId(0L);
+		libraryService.addStockByBookId(0L);
+		libraryService.addStockByBookId(0L);
+		libraryService.addStockByBookId(0L);
+		
+		Long aux5=libraryService.addStockByBookId(0L);
+		Long stock=5L;
+
+		assertEquals(stock,aux5);
+	}
 	
+	@Test
+	public void getStockByBookId() {
+		LibraryService libraryService= LibraryService.getInstance();
+
+	}
+	
+	@Test
+	public void removeStockByBookId() {
+		LibraryService libraryService= LibraryService.getInstance();
+
+	}	
 }
