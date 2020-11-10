1ccbed1d82162122444173fdd1aed521c91e6157
marcelo1995tate <mzelaya@alumno.unlam.edu.ar>
2020-11-09 21:51:23 -0300

    findByIdTest-OK

--- src/test/java/com/unlam/library/services/AuthorServiceTest.java
+++ src/test/java/com/unlam/library/services/AuthorServiceTest.java
@@ -2,6 +2,8 @@ package com.unlam.library.services;
 
 import static org.junit.Assert.*;
 
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,28 +16,31 @@ public class AuthorServiceTest {
 		AuthorService authorService=AuthorService.getInstance();
 		authorService.cleanAuthor();
 	}
+	
 	@Test
 	public void upSert() {
 		AuthorService authorService=AuthorService.getInstance();
-		Person person=new Person();
-		PersonService personService=PersonService.getInstance();
-		personService.upsert(person);
 		Author author=new Author();
 		
 		Author aux=authorService.upsert(author);
-		Long add=1L;
-		
+		Long add=0L;
+	
 		assertEquals(add,aux.getId());
 	}
 	
 	@Test
 	public void findById() {
 		AuthorService authorService=AuthorService.getInstance();
-		Person person=new Person();
-		PersonService personService=PersonService.getInstance();
-		personService.upsert(person);
 		Author author=new Author();
+		
+		Author aux=authorService.upsert(author);
+		System.out.println(aux.getId());
+		
+		assertEquals(Optional.empty(),authorService.findById(15l));
+		assertEquals(aux.getId(),authorService.findById(1l).get().getId());
+
 	}
+	
 	@Test
 	public void delete() {
 		AuthorService authorService=AuthorService.getInstance();
